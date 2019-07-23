f1e1aa31707c5e32a08ecc5c536442655258591e
David Clemens <davidlclemens@hotmail.com>
2019-07-23 09:51:39 -0500

    Dictionary Exercise added
    Completed dictionary exercise and am uploading the file.  also got rid of test file and updated bookmark.

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['one'] = "This is one."
+dict[2]    = "This is two."
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print(dict['one'])       #Prints teh value for the 'one' key
+print(dict[2])           #prints the value for the 2 key
+print(tinydict)          #prints the complete dictionary
+print(tinydict.keys())   #prints all the keys
+print(tinydict.values()) #prints all the values
