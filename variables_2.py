7222a6f5e58c6100bcb21a29e6cd964b2ae7e155
David Clemens <davidlclemens@hotmail.com>
2019-08-07 14:28:49 -0500

    Variables Exercise
    Updated where to start and finished the Variables tutorial from the new site.

---
+++ variables_2.py
@@ -0,0 +1,12 @@
+#  The target of this exercise is to create a string, an integer, and a floating point number.
+#  The string should be named mystring and should contain the word "hello".
+#  The floating point number should be named myfloat and should contain the number 10.0
+#  The integer should be named myint and should contain the number 20.
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+print(mystring)
+print(myfloat)
+print(myint)
